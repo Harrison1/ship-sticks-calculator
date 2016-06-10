2a019925b5d4ecaec65e423ec61c8acd7ece9514
Harrison <hmcguire13@gmail.com>
2016-06-10 14:24:57 -0700

    json

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
    request.open("GET", "products.json", false);
    request.send(null);
    var my_JSON_object = JSON.parse(request.responseText);
-   alert (my_JSON_object.result[0]);
+   alert (my_JSON_object.result[]);
 
 function calculateProduct() {
 	console.log("yoooooo");
