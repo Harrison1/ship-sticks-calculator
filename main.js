c9fa93e9b8683490c15985a2409345e2521da119
Harrison <hmcguire13@gmail.com>
2016-06-11 14:44:22 -0700

    edits

--- main.js
+++ main.js
@@ -20,32 +20,34 @@
    // var my_JSON_object = JSON.parse(request.responseText);
    // console.log(my_JSON_object);
 
-var data = '{ "products": [' +
-    '{"name": "Small Package", "type": "Golf", "length": 48, "width": 14, "height": 12, "weight": 42},' +
-    '{"name": "Large Package", "type": "Golf", "length": 52, "width": 16, "height": 14, "weight": 56},' +
-    '{"name": "Extra Large Package", "type": "Golf", "length": 56, "width": 18, "height": 16, "weight": 70},' +
-    '{"name": "Carry On", "type": "Luggage", "length": 25, "width": 15, "height": 7, "weight": 25},' +
-    '{"name": "Checked Bag", "type": "Luggage", "length": 26, "width": 16, "height": 22, "weight": 50},' +
-    '{"name": "Oversized Bag", "type": "Luggage", "length": 41, "width": 11, "height": 24, "weight": 72},' +
-    '{"name": "Ski Bag", "type": "Ski", "length": 72, "width": 8, "height": 10, "weight": 25},' +
-    '{"name": "Snowboard Bag", "type": "Ski", "length": 62, "width": 14, "height": 8, "weight": 25},' +
-    '{"name": "Double Ski Bag", "type": "Ski", "length": 80, "width": 10, "height": 12, "weight": 40},' +
-    '{"name": "Double Snowboard Bag", "type": "Ski", "length": 70, "width": 14, "height": 12, "weight": 40},' +
-    '{"name": "Snowboot Bag", "type": "Ski", "length": 25, "width": 15, "height": 7, "weight": 25} ]}';
+// var data = '{ "products": [' +
+//     '{"name": "Small Package", "type": "Golf", "length": 48, "width": 14, "height": 12, "weight": 42},' +
+//     '{"name": "Large Package", "type": "Golf", "length": 52, "width": 16, "height": 14, "weight": 56},' +
+//     '{"name": "Extra Large Package", "type": "Golf", "length": 56, "width": 18, "height": 16, "weight": 70},' +
+//     '{"name": "Carry On", "type": "Luggage", "length": 25, "width": 15, "height": 7, "weight": 25},' +
+//     '{"name": "Checked Bag", "type": "Luggage", "length": 26, "width": 16, "height": 22, "weight": 50},' +
+//     '{"name": "Oversized Bag", "type": "Luggage", "length": 41, "width": 11, "height": 24, "weight": 72},' +
+//     '{"name": "Ski Bag", "type": "Ski", "length": 72, "width": 8, "height": 10, "weight": 25},' +
+//     '{"name": "Snowboard Bag", "type": "Ski", "length": 62, "width": 14, "height": 8, "weight": 25},' +
+//     '{"name": "Double Ski Bag", "type": "Ski", "length": 80, "width": 10, "height": 12, "weight": 40},' +
+//     '{"name": "Double Snowboard Bag", "type": "Ski", "length": 70, "width": 14, "height": 12, "weight": 40},' +
+//     '{"name": "Snowboot Bag", "type": "Ski", "length": 25, "width": 15, "height": 7, "weight": 25} ]}';
 
+var obj;
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "products.json", true);
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
     // innerText does not let the attacker inject HTML elements.
-    var my_JSON_object = JSON.parse(xhr.responseText);
-    console.log(my_JSON_object);
-    console.log(my_JSON_object.products[0].name);
+    obj = JSON.parse(xhr.responseText);
+    console.log(obj);
+    console.log(obj[0].name);
   }
 }
 xhr.send();
 
-var obj = JSON.parse(data);
+console.log(obj);
+// var obj = JSON.parse(data);
 // console.log(obj.products);
 var productArray = [];
 var dStrings = ["length","width", "height", "weight"];
