44b7e3602e50fdad50e1e8508bde102498e474bb
Harrison <hmcguire13@gmail.com>
2016-06-11 14:46:07 -0700

    edits

--- main.js
+++ main.js
@@ -14,41 +14,16 @@
 //     }
 // }
 
-   // var request = new XMLHttpRequest();
-   // request.open("GET", "products.json", false);
-   // request.send(null);
-   // var my_JSON_object = JSON.parse(request.responseText);
-   // console.log(my_JSON_object);
-
-// var data = '{ "products": [' +
-//     '{"name": "Small Package", "type": "Golf", "length": 48, "width": 14, "height": 12, "weight": 42},' +
-//     '{"name": "Large Package", "type": "Golf", "length": 52, "width": 16, "height": 14, "weight": 56},' +
-//     '{"name": "Extra Large Package", "type": "Golf", "length": 56, "width": 18, "height": 16, "weight": 70},' +
-//     '{"name": "Carry On", "type": "Luggage", "length": 25, "width": 15, "height": 7, "weight": 25},' +
-//     '{"name": "Checked Bag", "type": "Luggage", "length": 26, "width": 16, "height": 22, "weight": 50},' +
-//     '{"name": "Oversized Bag", "type": "Luggage", "length": 41, "width": 11, "height": 24, "weight": 72},' +
-//     '{"name": "Ski Bag", "type": "Ski", "length": 72, "width": 8, "height": 10, "weight": 25},' +
-//     '{"name": "Snowboard Bag", "type": "Ski", "length": 62, "width": 14, "height": 8, "weight": 25},' +
-//     '{"name": "Double Ski Bag", "type": "Ski", "length": 80, "width": 10, "height": 12, "weight": 40},' +
-//     '{"name": "Double Snowboard Bag", "type": "Ski", "length": 70, "width": 14, "height": 12, "weight": 40},' +
-//     '{"name": "Snowboot Bag", "type": "Ski", "length": 25, "width": 15, "height": 7, "weight": 25} ]}';
-
 var obj;
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "products.json", true);
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
-    // innerText does not let the attacker inject HTML elements.
     obj = JSON.parse(xhr.responseText);
-    console.log(obj);
-    console.log(obj[0].name);
   }
 }
 xhr.send();
 
-console.log(obj);
-// var obj = JSON.parse(data);
-// console.log(obj.products);
 var productArray = [];
 var dStrings = ["length","width", "height", "weight"];
 
@@ -147,14 +122,9 @@ function removeProduct(measurement, products, i, element) {
 }
 
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("launch-btn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
 	document.getElementById('length-input').value = '';
 	document.getElementById('width-input').value = '';
@@ -168,12 +138,10 @@ btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
