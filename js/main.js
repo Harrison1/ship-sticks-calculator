afaefc15730693a839220d4819e9d687c8b981fe
Harrison <hmcguire13@gmail.com>
2016-06-12 15:59:02 -0700

    comments

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var dStrings = ["length","width", "height", "weight"];
 document.getElementById("unscrolled-menu-wrapper").style.visibility = "visible";
 document.getElementById("navbar-menu").style.visibility = "hidden";
 
+// displays and hides navbar
 window.onscroll = function (e)
 {
     scrollPosition = document.body.scrollTop;  
@@ -19,6 +20,7 @@ window.onscroll = function (e)
     }
 }
 
+// grab data
 var obj;
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "./data/products.json", true);
@@ -29,6 +31,7 @@ xhr.onreadystatechange = function() {
 }
 xhr.send();
 
+// main function to calculate the answer
 function calculate(length, width, height, weight) {
 	if(length>0 && width>0 && height>0 && weight>0) {
 		productArray = []
@@ -80,6 +83,7 @@ function calculate(length, width, height, weight) {
 
 }
 
+// grabs the values from the input boxes and begins the calculate function. 
 function calculateProduct() {
 	if(document.getElementById('length-input').value && document.getElementById('width-input').value && document.getElementById('height-input').value && document.getElementById('weight-input').value) {
 		document.getElementById("error").style.visibility = "hidden";
@@ -106,6 +110,7 @@ function calculateProduct() {
 
 }
 
+// function to remove products from custom array that do not match the criteria
 function removeProduct(measurement, products, i, element) {
 	i = productArray.length;
 	if(i) {
@@ -120,6 +125,7 @@ function removeProduct(measurement, products, i, element) {
 	}
 }
 
+// modal functions to display and hide modal
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("launch-btn");
 var span = document.getElementsByClassName("close")[0];
@@ -147,6 +153,7 @@ window.onclick = function(event) {
     }
 }
 
+// modal to close the function and display answer on home page
 function closeModal() {
 	modal.style.display = "none";
 	if(document.getElementById("answer").style.visibility == "visible") {
